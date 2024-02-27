41284a24055a198edb65cb8a42780c205a7fe677
Garfield Lee Freeman <gfreeman@paloaltonetworks.com>
2024-02-27 20:17:16 +0100

    generic helper to load a file into an interface

---
+++ pkg/load/file.go
@@ -0,0 +1,27 @@
+package load
+
+import (
+    "encoding/json"
+    "fmt"
+    "strings"
+
+    "gopkg.in/yaml.v3"
+)
+
+
+func File(v []byte, o interface{}) error {
+    var err error
+
+    runes := []byte(strings.TrimSpace(string(v)))
+    if len(runes) == 0 {
+        return fmt.Errorf("no data in file")
+    }
+
+    if runes[0] == '{' && runes[len(runes)-1] == '}' {
+        err = json.Unmarshal(v, o)
+    } else {
+        err = yaml.Unmarshal(v, o)
+    }
+
+    return err
+}
