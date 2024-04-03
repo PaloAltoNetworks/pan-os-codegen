d31fe9c81ab849041846350b86989d3763478b86
Sebastian Czech <sczech@paloaltonetworks.com>
2024-04-03 23:50:17 +0200

    Do not introduce short names for imports

--- pkg/translate/imports.go
+++ pkg/translate/imports.go
@@ -9,22 +9,22 @@ func RenderImports(templateType string) (string, error) {
 	case "entry":
 		manager.AddStandardImport("encoding/xml", "")
 		manager.AddStandardImport("fmt", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "filtering")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/generic", "generic")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "util")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "version")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/generic", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "")
 	case "location":
 		manager.AddStandardImport("fmt", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "errors")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "util")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "version")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "")
 	case "service":
 		manager.AddStandardImport("context", "")
 		manager.AddStandardImport("fmt", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "errors")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "filtering")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "util")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/xmlapi", "xmlapi")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
+		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/xmlapi", "")
 	}
 
 	return manager.RenderImports()
