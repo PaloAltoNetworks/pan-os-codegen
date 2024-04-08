4c51391cfbede145128c067296c1bbd0c8ecc8f3
Sebastian Czech <sczech@paloaltonetworks.com>
2024-04-08 11:37:51 +0200

    Refactor imports.go

--- pkg/translate/imports.go
+++ pkg/translate/imports.go
@@ -3,30 +3,32 @@ package translate
 import "github.com/paloaltonetworks/pan-os-codegen/pkg/imports"
 
 // RenderImports render string, which contains import required in entry, location or service template.
-func RenderImports(templateType string) (string, error) {
+func RenderImports(templateTypes ...string) (string, error) {
 	manager := imports.NewManager()
 
-	switch templateType {
-	case "entry":
-		manager.AddStandardImport("encoding/xml", "")
-		manager.AddStandardImport("fmt", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/generic", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "")
-	case "location":
-		manager.AddStandardImport("fmt", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "")
-	case "service":
-		manager.AddStandardImport("context", "")
-		manager.AddStandardImport("fmt", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/xmlapi", "")
-	case "filtering":
-		manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "")
+	for _, templateType := range templateTypes {
+		switch templateType {
+		case "entry":
+			manager.AddStandardImport("encoding/xml", "")
+			manager.AddStandardImport("fmt", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/generic", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "")
+		case "location":
+			manager.AddStandardImport("fmt", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/version", "")
+		case "service":
+			manager.AddStandardImport("context", "")
+			manager.AddStandardImport("fmt", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/errors", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/util", "")
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/xmlapi", "")
+		case "filtering":
+			manager.AddSdkImport("github.com/PaloAltoNetworks/pango/filtering", "")
+		}
 	}
 
 	return manager.RenderImports()
