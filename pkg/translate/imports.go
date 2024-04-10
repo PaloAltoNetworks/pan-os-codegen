f4aba9d0015b0dcf66d654ae450fdeb1697449b2
pimielowski <p.p.imielowski@gmail.com>
2024-04-10 10:56:25 +0200

    Update RenderImports to accept multiple template types
    The RenderImports function has been updated to accept an array of template types. This allows multiple templates to be processed in a single call, simplifying processing whilst retaining existing functionality. This change impacts the addTemplateImports function too, which now loops over each template type input.

--- pkg/translate/imports.go
+++ pkg/translate/imports.go
@@ -3,7 +3,7 @@ package translate
 import "github.com/paloaltonetworks/pan-os-codegen/pkg/imports"
 
 // RenderImports render string, which contains import required in entry, location or service template.
-func RenderImports(templateType string) (string, error) {
+func RenderImports(templateType ...string) (string, error) {
 	manager := imports.NewImportManager()
 
 	addTemplateImports(templateType, manager)
@@ -11,37 +11,39 @@ func RenderImports(templateType string) (string, error) {
 	return manager.RenderImports()
 }
 
-func addTemplateImports(templateType string, manager *imports.ImportManager) {
-	switch templateType {
-	case "entry":
-		manager.AddImport(imports.Standard, "encoding/xml", "")
-		manager.AddImport(imports.Standard, "fmt", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/filtering", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/generic", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/util", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/version", "")
-	case "location":
-		manager.AddImport(imports.Standard, "fmt", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/errors", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/util", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/version", "")
-	case "service":
-		manager.AddImport(imports.Standard, "context", "")
-		manager.AddImport(imports.Standard, "fmt", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/errors", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/filtering", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/util", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/xmlapi", "")
-	case "filtering":
-		manager.AddImport(imports.Sdk,"github.com/PaloAltoNetworks/pango/filtering","")
-	case "provider":
-		manager.AddImport(imports.Standard, "context", "")
-		manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango", "sdk")
-		manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/datasource", "")
-		manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/provider", "")
-		manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/provider/schema", "")
-		manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/resource", "")
-		manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/types", "")
-		manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-log/tflog", "")
+func addTemplateImports(templateType []string, manager *imports.ImportManager) {
+	for _, template := range templateType {
+		switch template {
+		case "entry":
+			manager.AddImport(imports.Standard, "encoding/xml", "")
+			manager.AddImport(imports.Standard, "fmt", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/filtering", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/generic", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/util", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/version", "")
+		case "location":
+			manager.AddImport(imports.Standard, "fmt", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/errors", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/util", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/version", "")
+		case "service":
+			manager.AddImport(imports.Standard, "context", "")
+			manager.AddImport(imports.Standard, "fmt", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/errors", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/util", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/xmlapi", "")
+		case "filtering":
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango/filtering", "")
+		case "provider":
+			manager.AddImport(imports.Standard, "context", "")
+			manager.AddImport(imports.Sdk, "github.com/PaloAltoNetworks/pango", "sdk")
+			manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/datasource", "")
+			manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/provider", "")
+			manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/provider/schema", "")
+			manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/resource", "")
+			manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-framework/types", "")
+			manager.AddImport(imports.Hashicorp, "github.com/hashicorp/terraform-plugin-log/tflog", "")
+		}
 	}
+
 }
