def31ffca82e5199290159a776b1a958bf66ece3
pimielowski <p.p.imielowski@gmail.com>
2024-04-17 13:24:16 +0200

    Refactor terraform resource implementation in golang package
    The refactoring simplifies the usage of template functions in terraform_resource.go by utilizing camel casing and restructuring resource object. Also, methods for creating, updating, reading, and deleting of the Terraform state were introduced, preparing for future data operations and error handling. Placeholder structs and functions were added for future completion.

--- pkg/translate/golang/terraform/terraform_resource.go
+++ pkg/translate/golang/terraform/terraform_resource.go
@@ -15,17 +15,10 @@ func Resource(spec *properties.Normalization, terraformProvider *properties.Terr
 	tfName := spec.Name
 	metaName := fmt.Sprintf("_%s", tfName)
 	structName := naming.CamelCase("", tfName, "Resource", false)
-	modelName := naming.CamelCase("", tfName, "RsModel", false)
-	newFuncName := naming.CamelCase("New", structName, "", true)
 
 	resourcetemplateFunctions := template.FuncMap{
-		"MetaName":         func() string { return metaName },
-		"StructName":       func() string { return structName },
-		"ModelName":        func() string { return modelName },
-		"NewFuncName":      func() string { return newFuncName },
-		"RepoShortName":    func() string { return spec.RepositoryShortName },
-		"ServiceShortName": func() string { return "fleflyf" },
-		"ProviderName":     func() string { return spec.Name },
+		"metaName":   func() string { return metaName },
+		"structName": func() string { return structName },
 	}
 	resourceTemplate := template.Must(
 		template.New(
@@ -37,11 +30,138 @@ func Resource(spec *properties.Normalization, terraformProvider *properties.Terr
 // Resource object
 
 var (
-	_ resource.Resource                = &nestedAddressObjectResource{}
-	_ resource.ResourceWithConfigure   = &nestedAddressObjectResource{}
-	_ resource.ResourceWithImportState = &nestedAddressObjectResource{}
+	_ resource.Resource                = &{{ structName }}ObjectResource{}
+	_ resource.ResourceWithConfigure   = &{{ structName }}ObjectResource{}
+	_ resource.ResourceWithImportState = &{{ structName }}ObjectResource{}
 )
 
+func New{{ structName }}Resource() resource.Resource {
+	return &{{ structName }}Resource{}
+}
+
+// ExampleResource defines the resource implementation.
+type {{ structName }}Resource struct {
+	client *pango.XmlApiClient
+}
+
+type {{ structName }}ObjectLocation struct {
+// TODO: Location struct via function
+}
+
+func (o *{{ structName }}ObjectLocation) IsValid() error {
+	if o.Name == "" {
+		return fmt.Errorf("name is unspecified")
+	}
+
+	return o.Location.IsValid()
+}
+
+type {{ structName }}EntryModel struct {
+// TODO: Entry model struct via function
+}
+
+type nestedLocationModel struct {
+// TODO: Nested Location model struct via function
+}
+
+type nestedVsysLocation struct {
+// TODO: Nested Vsys Location model struct via function
+}
+
+type nestedDeviceGroupLocation struct {
+// TODO: Nested DeviceGroup Location model struct via function
+}
+
+func (r *ExampleResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "{{ metaName }}"
+}
+
+func (r *ExampleResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+// TODO: Fill schema via function
+}
+
+func (r *ExampleResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	r.client = req.ProviderData.(*pango.XmlApiClient)
+	
+	//TODO: There should be some error handling
+	//if !ok {
+	//	resp.Diagnostics.AddError(
+	//		"Unexpected Resource Configure Type",
+	//		fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+	//	)
+	//
+	//	return
+	//}
+	//
+	//r.client = client
+}
+
+func (r *ExampleResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data {{ structName }}EntryModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Write logs using the tflog package
+	// Documentation: https://terraform.io/plugin/log
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ExampleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data {{ structName }}EntryModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ExampleResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data {{ structName }}EntryModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *ExampleResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data {{ structName }}EntryModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+}
+
+func (r *ExampleResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("tfid"), req, resp)
+}
 
 {{- /* Done */ -}}`,
 		),
