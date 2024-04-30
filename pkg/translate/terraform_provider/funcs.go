3e291ce3d7a2a640a3d3a5a6fcd986fcd4603caf
pimielowski <p.p.imielowski@gmail.com>
2024-04-30 15:15:50 +0200

    Enhance Terraform provider translation and model generation

--- pkg/translate/terraform_provider/funcs.go
+++ pkg/translate/terraform_provider/funcs.go
@@ -14,7 +14,6 @@ func ParamToModel(paramName string, paramProp properties.TerraformProviderParams
 		"UnderscoreName": func() string { return naming.Underscore("", paramName, "") },
 		"CamelCaseType":  func() string { return naming.CamelCase("", paramProp.Type, "", true) },
 	}
-
 	modelTemplate := template.Must(
 		template.New(
 			"param-to-model",
@@ -26,19 +25,15 @@ func ParamToModel(paramName string, paramProp properties.TerraformProviderParams
 {{- /* Done */ -}}`,
 		),
 	)
-
 	var builder strings.Builder
 	err := modelTemplate.Execute(&builder, paramProp)
-
 	return builder.String(), err
 }
-
 func ParamToSchema(paramName string, paramProp properties.TerraformProviderParams) (string, error) {
 	funcMap := template.FuncMap{
 		"AttName":       func() string { return paramName },
 		"CamelCaseType": func() string { return naming.CamelCase("", paramProp.Type, "", true) },
 	}
-
 	schemaTemplate := template.Must(
 		template.New(
 			"describe-param",
@@ -61,16 +56,12 @@ func ParamToSchema(paramName string, paramProp properties.TerraformProviderParam
 {{- /* Done */ -}}`,
 		),
 	)
-
 	var builder strings.Builder
 	err := schemaTemplate.Execute(&builder, paramProp)
-
 	return builder.String(), err
 }
-
 func TfidStruct(structType string, structName string) (string, error) {
 	var tfidStructTemplate *template.Template
-
 	if structType == "entry" {
 		funcMap := template.FuncMap{}
 		tfidStructTemplate = template.Must(
@@ -80,15 +71,93 @@ func TfidStruct(structType string, structName string) (string, error) {
 				funcMap,
 			).Parse(`
 {{- /* Begin */ -}}
-	Name     string          ` + "`" + `json:"name" ` + "`" + `
-	Location ` + fmt.Sprintf(`%s.Location `, structName) + "`" + `json:"name" ` + "`" + `
+	Name     string          ` + "`" + `json:"name"` + "`" + `
+	Location ` + fmt.Sprintf(`%s.Location `, structName) + "`" + `json:"location"` + "`" + `
 {{- /* Done */ -}}`,
 			),
 		)
 	}
-
 	var builder strings.Builder
 	err := tfidStructTemplate.Execute(&builder, "")
-
 	return builder.String(), err
+}
+
+func ParamToModelResource(paramName string, paramProp *properties.SpecParam, structName string) (string, error) {
+	funcMap := template.FuncMap{
+		"CamelCaseName":  func() string { return naming.CamelCase("", paramName, "", true) },
+		"UnderscoreName": func() string { return naming.Underscore("", paramName, "") },
+		"CamelCaseType":  func() string { return naming.CamelCase("", paramProp.Type, "", true) },
+	}
+	var builder strings.Builder
+	var modelTemplate *template.Template
+	// if the parameter has nested parameters (type is empty)
+	if paramProp.Type == "" {
+		modelTemplate = template.Must(
+			template.New(
+				"param-to-model",
+			).Funcs(
+				funcMap,
+			).Parse(`
+           {{- /* Begin */ -}}
+           {{ "    " }}{{ CamelCaseName }} ` + structName + `{{ CamelCaseName }}Object ` + "`" + `tfsdk:"{{ UnderscoreName }}"` + "`" + `
+           {{- /* Done */ -}}`,
+			),
+		)
+	} else {
+		modelTemplate = template.Must(
+			template.New(
+				"param-to-model",
+			).Funcs(
+				funcMap,
+			).Parse(`
+           {{- /* Begin */ -}}
+           {{ "    " }}{{ CamelCaseName }} types.{{ CamelCaseType }} ` + "`" + `tfsdk:"{{ UnderscoreName }}"` + "`" + `
+           {{- /* Done */ -}}`,
+			),
+		)
+	}
+	err := modelTemplate.Execute(&builder, paramProp)
+	if err != nil {
+		return "", err
+	}
+	return builder.String(), nil
+}
+
+func TfidResourceModel(structType string, structName string) (string, error) {
+	var tfidStructTemplate *template.Template
+	if structType == "entry" {
+		funcMap := template.FuncMap{}
+		tfidStructTemplate = template.Must(
+			template.New(
+				"describe-param",
+			).Funcs(
+				funcMap,
+			).Parse(`
+{{- /* Begin */ -}}
+	Tfid     types.String          ` + "`" + `tfsdk:"tfid"` + "`" + `
+	Location ` + fmt.Sprintf(`%sLocation `, structName) + "`" + `tfsdk:"location"` + "`" + `
+{{- /* Done */ -}}`,
+			),
+		)
+	}
+	var builder strings.Builder
+	err := tfidStructTemplate.Execute(&builder, "")
+	return builder.String(), err
+}
+
+func CalculateModelStruct(paramName string, paramProp *properties.SpecParam, structName string) (string, error) {
+	if paramProp.Type == "" {
+		if paramProp.Spec.Params != nil {
+			for nestedIndex, nestedParam := range paramProp.Spec.Params {
+				return fmt.Sprintf("%s.%v", nestedIndex, nestedParam), nil
+			}
+		}
+		if paramProp.Spec.OneOf != nil {
+			for nestedIndex, nestedParam := range paramProp.Spec.OneOf {
+				return fmt.Sprintf("%s.%v", nestedIndex, nestedParam), nil
+			}
+		}
+
+	}
+	return "", nil
 }
