5e0826067984aac63dc24320e06f5c32a82ab725
Sebastian Czech <sczech@paloaltonetworks.com>
2024-05-13 14:12:31 +0200

    Basic logging for send data, received data and as curl commands with optional personal data

--- assets/sdk/xmlapiclient.go
+++ assets/sdk/xmlapiclient.go
@@ -8,6 +8,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
+	"log"
 	"mime"
 	"mime/multipart"
 	"net/http"
@@ -236,6 +237,9 @@ func (c *XmlApiClient) Setup() error {
 	}
 
 	// Logging.
+	if c.Logging == nil {
+		c.Logging = make(map[string]bool)
+	}
 	if len(c.Logging) == 0 {
 		if val := os.Getenv("PANOS_LOGGING"); c.CheckEnvironment && val != "" {
 			ll := strings.Split(val, ",")
@@ -865,6 +869,8 @@ func (c *XmlApiClient) Communicate(ctx context.Context, cmd util.PangoCommand, s
 		data.Set("key", c.ApiKey)
 	}
 
+	c.logSend(data)
+
 	req, err := http.NewRequestWithContext(ctx, "POST", c.api_url, strings.NewReader(data.Encode()))
 	if err != nil {
 		return nil, nil, err
@@ -890,7 +896,7 @@ func (c *XmlApiClient) ImportFile(ctx context.Context, cmd *xmlapi.Import, conte
 		data.Set("key", c.ApiKey)
 	}
 
-	//c.logSend(data)
+	c.logSend(data)
 
 	buf := bytes.Buffer{}
 	w := multipart.NewWriter(&buf)
@@ -1069,5 +1075,139 @@ func (c *XmlApiClient) sendRequest(ctx context.Context, req *http.Request, strip
 		return body, resp, fmt.Errorf("err unmarshaling into provided interface: %s", err)
 	}
 
+	c.logReceive(body)
+
 	return body, resp, nil
+}
+
+// logSend logs the data being sent by the XmlApiClient.
+//
+// It checks the Logging map to determine which type of logging to perform.
+// If Logging["send"] is true, it logs the data using the logSendData method.
+// If Logging["curl"] is true, it logs the data as a curl command using the logDataAsCurl method.
+// Finally, if there is any logged data, it prints it using log.Printf.
+func (c *XmlApiClient) logSend(data url.Values) {
+	b := &strings.Builder{}
+
+	if c.Logging["send"] {
+		c.logSendData(b, data)
+	}
+	if c.Logging["curl"] {
+		c.logDataAsCurl(b, data)
+	}
+
+	if b.Len() > 0 {
+		log.Printf("%s", b.String())
+	}
+}
+
+func (c *XmlApiClient) logSendData(b *strings.Builder, data url.Values) {
+	if b.Len() > 0 {
+		fmt.Fprintf(b, "\n")
+	}
+	realKey := data.Get("key")
+	if realKey != "" {
+		data.Set("key", "########")
+	}
+	fmt.Fprintf(b, "Sending data: %#v", data)
+	if realKey != "" {
+		data.Set("key", realKey)
+	}
+}
+
+func (c *XmlApiClient) logDataAsCurl(b *strings.Builder, data url.Values) {
+	if b.Len() > 0 {
+		fmt.Fprintf(b, "\n")
+	}
+	special := map[string]string{
+		"key":     "",
+		"element": "",
+	}
+	ev := url.Values{}
+	for k := range data {
+		var isSpecial bool
+		for sk := range special {
+			if sk == k {
+				isSpecial = true
+				special[k] = data.Get(k)
+				break
+			}
+		}
+		if !isSpecial {
+			ev[k] = make([]string, 0, len(data[k]))
+			for i := range data[k] {
+				ev[k] = append(ev[k], data[k][i])
+			}
+		}
+	}
+
+	// Build up the curl command.
+	fmt.Fprintf(b, "curl")
+
+	// Skip cert verify.
+	if c.SkipVerifyCertificate {
+		fmt.Fprintf(b, " -k")
+	}
+	// Headers.
+	if len(c.Headers) > 0 && c.Logging["personal-data"] {
+		for k, v := range c.Headers {
+			if v != "" {
+				fmt.Fprintf(b, " --header '%s: %s'", k, v)
+			} else {
+				fmt.Fprintf(b, " --header '%s;'", k)
+			}
+		}
+	}
+	// Add URL encoded values.
+	if special["key"] != "" {
+		if c.Logging["personal-data"] {
+			ev.Set("key", special["key"])
+		} else {
+			ev.Set("key", "APIKEY")
+		}
+	}
+	// Add in the element, if present.
+	if special["element"] != "" {
+		fmt.Fprintf(b, " --data-urlencode element@element.xml")
+	}
+	// URL.
+	fmt.Fprintf(b, " '%s://", c.Protocol)
+	if c.Logging["personal-data"] {
+		fmt.Fprintf(b, "%s", c.Hostname)
+	} else {
+		fmt.Fprintf(b, "HOST")
+	}
+	if c.Port != 0 {
+		fmt.Fprintf(b, ":%d", c.Port)
+	}
+	fmt.Fprintf(b, "/api")
+	if len(ev) > 0 {
+		fmt.Fprintf(b, "?%s", ev.Encode())
+	}
+	fmt.Fprintf(b, "'")
+
+	// Data.
+	if special["element"] != "" {
+		fmt.Fprintf(b, "\nelement.xml:\n%s", special["element"])
+	}
+}
+
+func (c *XmlApiClient) logReceive(body []byte) {
+	b := &strings.Builder{}
+
+	if c.Logging["receive"] {
+		c.logReceiveData(b, body)
+	}
+
+	if b.Len() > 0 {
+		log.Printf("%s", b.String())
+	}
+}
+
+// logReceiveData logs the received data.
+func (c *XmlApiClient) logReceiveData(b *strings.Builder, body []byte) {
+	if b.Len() > 0 {
+		fmt.Fprintf(b, "\n")
+	}
+	fmt.Fprintf(b, "Received data: %s", body)
 }
