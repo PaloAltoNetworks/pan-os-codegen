8bf447eb62e5336be878024ddc3bc4ea70ca6b1a
Krzysztof Klimonda <kklimonda@paloaltonetworks.com>
2024-08-15 12:29:28 +0200

    Drop "UseStateForUnknown" UUID modifier
    That doesn't work well with movements, should be investigated later post MVP.

--- pkg/translate/terraform_provider/funcs.go
+++ pkg/translate/terraform_provider/funcs.go
@@ -940,29 +940,17 @@ func createSchemaAttributeForParameter(schemaTyp schemaType, packageName string,
 		computed = true
 	}
 
-	// TODO(kklimonda): This is pretty one-off implementation to
-	// support uuid-style resources, but could be expanded to be more
-	// generic if needed.
-	var modifiers *modifierCtx
-	if schemaTyp == schemaResource && computed && param.Default == "" {
-		modifiers = &modifierCtx{
-			SchemaType: fmt.Sprintf("planmodifier.%s", pascalCase(param.Type)),
-			Modifiers:  []string{fmt.Sprintf("%splanmodifier.UseStateForUnknown()", param.Type)},
-		}
-	}
-
 	return attributeCtx{
-		Package:       packageName,
-		Name:          param.Name,
-		SchemaType:    schemaType,
-		ElementType:   elementType,
-		Description:   param.Description,
-		Required:      param.Required,
-		Optional:      !param.Required,
-		Sensitive:     param.Sensitive,
-		Default:       defaultValue,
-		Computed:      computed,
-		PlanModifiers: modifiers,
+		Package:     packageName,
+		Name:        param.Name,
+		SchemaType:  schemaType,
+		ElementType: elementType,
+		Description: param.Description,
+		Required:    param.Required,
+		Optional:    !param.Required,
+		Sensitive:   param.Sensitive,
+		Default:     defaultValue,
+		Computed:    computed,
 	}
 }
 
