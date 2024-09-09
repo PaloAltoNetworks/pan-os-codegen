b45ab80aaa1753d0bf1f1d5ce7623fe259f8e280
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2024-09-09 11:53:56 +0100

    add function example

--- assets/terraform/examples/functions/address_value/function.tf
+++ assets/terraform/examples/functions/address_value/function.tf
@@ -10,9 +10,19 @@ resource "panos_address_objects" "name" {
       description = "foo"
       ip_netmask  = "1.1.1.1"
     }
+    "bar" = {
+      description = "foo"
+      ip_netmask  = "2.2.2.2"
+    }
   }
 }
 
-output "address_value" {
+# Example 1: Get the value of a single address object.
+output "foo_value" {
   value = provider::panos::address_value(panos_address_objects.name.addresses.foo)
+}
+
+# Example 2: Transform all the address objects into a map of values.
+output "address_values" {
+  value = { for k, v in panos_address_objects.name.addresses : k => provider::panos::address_value(panos_address_objects.name.addresses[k]) }
 }
