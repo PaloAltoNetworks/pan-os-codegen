a5fb7cec6c3e5ef69b0722e7eb86a1faf0897a0f
Krzysztof Klimonda <kklimonda@paloaltonetworks.com>
2024-10-10 16:51:11 +0200

    Fix acceptance tests that no longer compiled

--- assets/terraform/test/resource_ethernet_interface_test.go
+++ assets/terraform/test/resource_ethernet_interface_test.go
@@ -70,7 +70,7 @@ func makePanosEthernetInterface_Layer3(label string) string {
     variable "ip_addr_netmask" { type = string }
     variable "template_name" { type = string }
 
-    resource "panos_panorama_template" "acc_codegen_template" {
+    resource "panos_template" "acc_codegen_template" {
         name = "${var.template_name}-${var.name_suffix}"
 
         location = {
@@ -84,7 +84,7 @@ func makePanosEthernetInterface_Layer3(label string) string {
       location = {
         template = {
           vsys = "vsys1"
-          name = panos_panorama_template.acc_codegen_template.name
+          name = panos_template.acc_codegen_template.name
         }
       }
 
@@ -141,4 +141,3 @@ func testAccCheckPanosEthernetInterfaceDestroy(entryName, templateName string) f
 		return nil
 	}
 }
-
