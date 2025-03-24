82ef3ad8d3ec24d4d4cfab250e6706995afcb79f
Krzysztof Klimonda <kklimonda@paloaltonetworks.com>
2025-03-24 14:41:15 +0100

    feat(docs): expand example for panos_address_group resource

--- assets/terraform/examples/resources/panos_address_group/resource.tf
+++ assets/terraform/examples/resources/panos_address_group/resource.tf
@@ -7,26 +7,37 @@ resource "panos_address_group" "example" {
 
   name        = "example-address-group"
   description = "example address group"
-  static      = [for k, v in panos_addresses.example.addresses : k]
+  static      = [for k in panos_address.example : k.name]
 }
 
-resource "panos_addresses" "example" {
+resource "panos_device_group" "example" {
+  location = {
+    panorama = {}
+  }
+
+  name = "example-device-group"
+}
+
+resource "panos_address" "example" {
   location = {
     device_group = {
       name = panos_device_group.example.name
     }
   }
 
-  addresses = {
-    "foo" = {
-      description = "foo example"
-      ip_netmask  = "1.1.1.1"
+  for_each = tomap({
+    "addr1" = {
+      description = "example address 1"
+      ip_netmask  = "10.0.0.1/32"
     }
-    "bar" = {
-      description = "bar example"
-      ip_netmask  = "2.2.2.2"
+    "addr2" = {
+      description = "example address 2"
+      fqdn        = "example.com"
     }
-  }
+  })
+
+  name        = each.key
+  description = each.value.description
+  ip_netmask  = lookup(each.value, "ip_netmask", null)
+  fqdn        = lookup(each.value, "fqdn", null)
 }
-
-
