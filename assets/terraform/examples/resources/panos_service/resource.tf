76bb64908965f44b7b28e7d7728a01bedcf2bbe2
Krzysztof Klimonda <kklimonda@paloaltonetworks.com>
2025-03-24 14:54:59 +0100

    feat(docs): Expand panos_service example

--- assets/terraform/examples/resources/panos_service/resource.tf
+++ assets/terraform/examples/resources/panos_service/resource.tf
@@ -11,7 +11,19 @@ resource "panos_service" "example" {
   protocol = {
     tcp = {
       destination_port = "80"
+      override = {
+        timeout           = 600
+        halfclose_timeout = 300
+        timewait_timeout  = 60
+      }
     }
   }
+}
 
+resource "panos_device_group" "example" {
+  location = {
+    panorama = {}
+  }
+
+  name = "example-device-group"
 }
