a8ec569a42d65045907135134f1f77107f1c9680
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2025-03-25 21:58:12 +0000

    fix example

--- assets/terraform/examples/functions/address_value/function.tf
+++ assets/terraform/examples/functions/address_value/function.tf
@@ -1,3 +1,13 @@
+# Example 1: Get the value of a single address object.
+output "foo_value" {
+  value = provider::panos::address_value(panos_addresses.example.addresses.foo)
+}
+
+# Example 2: Transform all the address objects into a map of values.
+output "address_values" {
+  value = { for k, v in panos_addresses.example.addresses : k => provider::panos::address_value(panos_addresses.example.addresses[k]) }
+}
+
 resource "panos_addresses" "example" {
   location = {
     device_group = {
@@ -17,12 +27,10 @@ resource "panos_addresses" "example" {
   }
 }
 
-# Example 1: Get the value of a single address object.
-output "foo_value" {
-  value = provider::panos::address_value(panos_addresses.example.addresses.foo)
-}
+resource "panos_device_group" "example" {
+  location = {
+    panorama = {}
+  }
 
-# Example 2: Transform all the address objects into a map of values.
-output "address_values" {
-  value = { for k, v in panos_addresses.example.addresses : k => provider::panos::address_value(panos_addresses.example.addresses[k]) }
+  name = "example-device-group"
 }
