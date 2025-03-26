4ae2a83749e8a58c29bb77664cfe0dc508aebe0b
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2025-03-26 11:06:31 +0000

    new examples

---
+++ assets/terraform/examples/resources/panos_address/resource.tf
@@ -0,0 +1,31 @@
+resource "panos_address" "example" {
+  location = {
+    device_group = {
+      name = panos_device_group.example.name
+    }
+  }
+
+  for_each = tomap({
+    "addr1" = {
+      description = "example address 1"
+      ip_netmask  = "10.0.0.1/32"
+    }
+    "addr2" = {
+      description = "example address 2"
+      fqdn        = "example.com"
+    }
+  })
+
+  name        = each.key
+  description = each.value.description
+  ip_netmask  = lookup(each.value, "ip_netmask", null)
+  fqdn        = lookup(each.value, "fqdn", null)
+}
+
+resource "panos_device_group" "example" {
+  location = {
+    panorama = {}
+  }
+
+  name = "example-device-group"
+}
