3153e8b2956d1b136449c9eb7979a7327f0c9abb
kklimonda-cl <kklimonda@paloaltonetworks.com>
2025-11-25 11:38:47 +0100

    fix(specs): Change schema for template stack devices (#677)

--- assets/terraform/examples/resources/panos_template_stack/resource.tf
+++ assets/terraform/examples/resources/panos_template_stack/resource.tf
@@ -1,23 +1,48 @@
 resource "panos_template_stack" "example" {
-
   location = {
     panorama = {}
   }
-  name        = "tempalte-stack-example"
+
+  name        = "example-template-stack"
   description = "example template stack"
 
   templates = [
     panos_template.example.name
   ]
 
+  # devices = [
+  #   { name = panos_firewall_device.fw1.name },
+  #   { name = panos_firewall_device.fw2.name }
+  # ]
+
+  # default_vsys = "vsys1"
 }
 
 resource "panos_template" "example" {
-
   location = {
     panorama = {}
   }
-  name        = "template-example"
-  description = "example template stack"
 
+  name        = "example-template"
+  description = "example template"
 }
+
+# resource "panos_firewall_device" "fw1" {
+#   location = {
+#     panorama = {}
+#   }
+#
+#   name     = "007200001234"
+#   hostname = "fw1.example.com"
+#   ip       = "192.0.2.1"
+# }
+#
+# resource "panos_firewall_device" "fw2" {
+#   location = {
+#     panorama = {}
+#   }
+#
+#   name     = "007200005678"
+#   hostname = "fw2.example.com"
+#   ip       = "192.0.2.2"
+# }
