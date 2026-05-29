f8c6679a9ccf6e19172fc9bd4c18bac7f43af7c0
arlohol <arlohol@cdw.com>
2026-05-29 08:24:02 -0400

    Adding tests for managed-by

--- assets/terraform/test/resource_general_settings_test.go
+++ assets/terraform/test/resource_general_settings_test.go
@@ -197,6 +197,136 @@ resource "panos_ntp_settings" "example" {
 }
 `
 
+const generalSettings_PanoramaManagedBy_Tmpl = `
+variable "create_template" { type = any }
+variable "location" { type = any }
+variable "prefix" { type = string }
+variable "managed_by" { type = string }
+
+resource "panos_template" "example" {
+  count = var.create_template ? 1 : 0
+  location = { panorama = {} }
+
+  name = format("%s-tmpl", var.prefix)
+}
+
+resource "panos_general_settings" "example" {
+  depends_on = [panos_template.example]
+
+  location = var.location
+
+  panorama = {
+    managed_by = var.managed_by
+  }
+}
+`
+
+func TestAccGeneralSettings_PanoramaManagedBy_System(t *testing.T) {
+	t.Parallel()
+
+	nameSuffix := acctest.RandStringFromCharSet(6, acctest.CharSetAlphaNum)
+	prefix := fmt.Sprintf("test-acc-%s", nameSuffix)
+
+	location := config.ObjectVariable(map[string]config.Variable{
+		"system": config.ObjectVariable(map[string]config.Variable{}),
+	})
+
+	createTemplate := config.BoolVariable(false)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: generalSettings_PanoramaManagedBy_Tmpl,
+				ConfigVariables: map[string]config.Variable{
+					"prefix":          config.StringVariable(prefix),
+					"location":        location,
+					"create_template": createTemplate,
+					"managed_by":      config.StringVariable("panorama"),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"panos_general_settings.example",
+						tfjsonpath.New("panorama").AtMapKey("managed_by"),
+						knownvalue.StringExact("panorama"),
+					),
+				},
+			},
+			{
+				Config: generalSettings_PanoramaManagedBy_Tmpl,
+				ConfigVariables: map[string]config.Variable{
+					"prefix":          config.StringVariable(prefix),
+					"location":        location,
+					"create_template": createTemplate,
+					"managed_by":      config.StringVariable("cloud-service"),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"panos_general_settings.example",
+						tfjsonpath.New("panorama").AtMapKey("managed_by"),
+						knownvalue.StringExact("cloud-service"),
+					),
+				},
+			},
+		},
+	})
+}
+
+func TestAccGeneralSettings_PanoramaManagedBy_Template(t *testing.T) {
+	t.Parallel()
+
+	nameSuffix := acctest.RandStringFromCharSet(6, acctest.CharSetAlphaNum)
+	prefix := fmt.Sprintf("test-acc-%s", nameSuffix)
+
+	location := config.ObjectVariable(map[string]config.Variable{
+		"template": config.ObjectVariable(map[string]config.Variable{
+			"name": config.StringVariable(prefix),
+		}),
+	})
+
+	createTemplate := config.BoolVariable(true)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: generalSettings_PanoramaManagedBy_Tmpl,
+				ConfigVariables: map[string]config.Variable{
+					"prefix":          config.StringVariable(prefix),
+					"location":        location,
+					"create_template": createTemplate,
+					"managed_by":      config.StringVariable("panorama"),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"panos_general_settings.example",
+						tfjsonpath.New("panorama").AtMapKey("managed_by"),
+						knownvalue.StringExact("panorama"),
+					),
+				},
+			},
+			{
+				Config: generalSettings_PanoramaManagedBy_Tmpl,
+				ConfigVariables: map[string]config.Variable{
+					"prefix":          config.StringVariable(prefix),
+					"location":        location,
+					"create_template": createTemplate,
+					"managed_by":      config.StringVariable("cloud-service"),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"panos_general_settings.example",
+						tfjsonpath.New("panorama").AtMapKey("managed_by"),
+						knownvalue.StringExact("cloud-service"),
+					),
+				},
+			},
+		},
+	})
+}
+
 func TestAccGeneralSettings_Sanity_Delete(t *testing.T) {
 	t.Parallel()
 
